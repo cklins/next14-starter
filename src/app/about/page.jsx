019ac420a8b87156987e9f054036712c58844843
cklin <cklin@itri.org.tw>
2023-12-29 15:46:48 +0800

    layout & app route

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+import styles from "./about.module.css";
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
---
+++ src/app/error.jsx
@@ -0,0 +1,8 @@
+"use client";
+
+const error = (params) => {
+  console.log(params);
+  return <div>error</div>;
+};
+
+export default error;
